55b0e6fb6b6965c6d3e9d8ddc2a3b3477bedcb00
Sezgin Gündogdu <sgbewerbung@yahoo.de>
2023-09-25 13:27:16 +0200

    %

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import { UserComponent } from './user/user.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    DialogAddUserComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    MatProgressBarModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]<i class="fa fa-y-combinator" aria-hidden="true"></i>
+})
+export class AppModule { }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { FirebaseserviceService } from '../firebaseservice.service';
+import { User } from '../models/user.class';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+
+    user = new User();
+    
+      constructor(public dialog: MatDialog, public serviceFire: FirebaseserviceService) {}
+    
+      positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+      position = new FormControl(this.positionOptions[2]);
+    
+      
+      openDialog(): void {
+        const dialogRef = this.dialog.open(DialogAddUserComponent, { //hier fügen wir die Komponente ein die geöffnet werden soll. In diesem Fall, "DialogAddUserComponent", weil das unser Dialog enthalten ist.
+          //data: {name: this.name},
+        });
+        
+      }
+
+}
