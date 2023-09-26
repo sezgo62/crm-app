73525beeef1a9ae39306f86ca5e7216e07372699
Sezgin Gündogdu <sgbewerbung@yahoo.de>
2023-09-26 06:52:18 +0200

    %

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDt8AsCpctEJNqcIYZ6MNsQUBiXfQLLPYM",
+    authDomain: "simple-crm-4e1b2.firebaseapp.com",
+    projectId: "simple-crm-4e1b2",
+    storageBucket: "simple-crm-4e1b2.appspot.com",
+    messagingSenderId: "740423993566",
+    appId: "1:740423993566:web:44fcab39e3827181599a4f",
+    measurementId: "G-S3GXBDH6XB"
+  };
